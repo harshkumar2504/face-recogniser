48008b594c763834f8a51b623a0f210cc4ad7f23
harshkumar2504 <109737523+harshkumar2504@users.noreply.github.com>
2023-09-22 09:54:18 +0530

    Update FaceRecognition.js

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './FaceRecognition.css'
-
-const FaceRecognition = ({image, box}) => {
-	return(
-		<div className ='center ma'>
-			<div className='absolute mt2'>
-				<img id='inputimage' alt='' src={image} width='500px' height='auto'/>
-				<div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-			</div>
-		</div>
-		)
+const FaceRecognition = (props) => {
+const box = props.box;
+  return (
+    <div style={{display:'flex', justifyContent:'center'}}>
+      <div className= "absolute pa2 " style={{width:'500px',height:'auto'}}>
+        <img id = "inputimage" src={props.imageUrl} alt="to recognize" />
+        <div className="boundingBox" style={{top:box.topRow , right:box.rightCol , bottom:box.bottomRow , left:box.leftCol}}></div>
+      </div>
+    </div>
+  )
 }
 
 export default FaceRecognition;
