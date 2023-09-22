84f5f9f00ff371dceca991e7db0cb0e439acbf36
harshkumar2504 <109737523+harshkumar2504@users.noreply.github.com>
2023-09-22 09:55:43 +0530

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import './Form.css'
+import './ImageLinkForm.css'
+const ImageLinkForm = (props) => {
 
-const Form = ({ onInputChange, onButtonSubmit }) => {
-	return (
-		<div>
-			<p className='f3'>
-				{'Permite detectar caras nas fotografias que inserires'}
-			</p>
-			<div className='center'>
-				<div className='center pa4 br3 shadow-5 form'>
-					<input className = 'f4 pa2 w-65 center' type='text' onChange={onInputChange}/>
-					<button className = 'w-35 grow f4 link ph3 pv2 dib white bg-black' onClick={onButtonSubmit} >Detectar</button>
-				</div>
-			</div>
-		</div>
-	)
+  const onInputChange = props.onInputChange;
+  const onSubmit = props.onSubmit;
+  return (
+    <div >
+      <p>
+          Dark magic happens bellow
+      </p>
+      <div  style = {{ display: 'flex', justifyContent:'center'}}>
+        <div className=" shadow-2 pa4 w-60 form" >
+          <input className="f4  w-70 pa2" type="text" onChange={onInputChange} defaultValue ="https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528"></input>
+          <button className="f3 grow w-30 link white dib bg-light-purple ph3 pv2" onClick={onSubmit}>Detect</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Form;
+export default ImageLinkForm;
