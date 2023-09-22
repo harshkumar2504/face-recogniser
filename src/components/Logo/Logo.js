dd41e320ab4d4bd99330a1d7d6058c198e302be4
harshkumar2504 <109737523+harshkumar2504@users.noreply.github.com>
2023-09-22 10:00:07 +0530

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import Tilt from 'react-tilt';
-import './Logo.css';
-import logo from './faceID.png'
+import Tilt from 'react-tilt'
+import './Logo.css'
+import brain from './brain.png'
 const Logo = () => {
-	return (
-	<div className='ma4 mt0'>
-		<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-		 <div className="Tilt-inner">
-		 	<img alt='logo' src={logo} style={{paddingTop: '22px'}} />
-		 </div>
-		</Tilt>
-	</div>
-	)
+  return (
+    <div className ="pa3"  style = {{ display :'flex', justifyContent:'flex-start'}}>
+      <Tilt
+        tiltMaxAngleX={100}
+       tiltMaxAngleY={50}
+       perspective={100}
+       scale={1}
+       transitionSpeed={2000}
+       gyroscope={false}
+       className="tilt br2 shadow-2 dib"
+  >
+        <div className="br2 shadow-2 " style={{ height: '150px', width:'150px'}}>
+           <img  className="pa3 logo"src = {brain} alt =" logo"/>
+        </div>
+      </Tilt>
+      </div>
+  )
 }
 
-export default Logo;              
-
-
-
-
-
- 
+export default Logo;
